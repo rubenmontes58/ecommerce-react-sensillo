536c7e8b395750d34f498e0633794d4aadee5c78
Lenovo <thinkpad@Lenovos-MacBook-Pro.local>
2023-06-21 18:20:15 -0300

    ecommerce en react sencillot

---
+++ src/data.js
@@ -0,0 +1,79 @@
+
+
+const data = {
+    products:[
+    { 
+        id:1,
+      name: "Notebook Acer", 
+      precio: 150000, 
+      descripcion: "I5 16Gb Ram 480ssd", 
+      image: "https://http2.mlstatic.com/D_NQ_NP_787180-MLA54518190876_032023-O.webp" 
+    },
+    { 
+        id:2,
+      name: "Notebook HP", 
+      precio: 160000, 
+      descripcion: "I3 8Gb Ram 480ssd", 
+      image: "https://http2.mlstatic.com/D_NQ_NP_687703-MLA49657523861_042022-O.webp" 
+    },
+    { 
+        id:3,
+      name: "Notebook Lenovo", 
+      precio: 155000, 
+      descripcion: "I7 16Gb Ram 256ssd", 
+      image: "https://http2.mlstatic.com/D_NQ_NP_745797-MLA69340654813_052023-O.webp" 
+    },
+    { 
+        id:4,
+      name: "Notebook Asus", 
+      precio: 158000, 
+      descripcion: "I5 16Gb Ram 480ssd", 
+      image:"https://http2.mlstatic.com/D_NQ_NP_824256-MLA51634174013_092022-O.webp" 
+    },
+    { 
+        id:5,
+      name: "Mouse Logitech", 
+      precio: 11500, 
+      descripcion: "Bluethooth Gamer", 
+      image:"https://http2.mlstatic.com/D_NQ_NP_918568-MLA32146214305_092019-O.webp" 
+    },
+    { 
+        id:6,
+      name: "Teclado Genius", 
+      precio: 13000, 
+      descripcion: "USB", 
+      image:"https://http2.mlstatic.com/D_NQ_NP_836343-MLA32722027759_102019-O.webp" 
+    },
+    { 
+        id:7,
+      name: "Monitor LG", 
+      precio: 125000, 
+      descripcion: "Hdmi 1080 24 pulgadas", 
+      image:"https://http2.mlstatic.com/D_NQ_NP_845070-MLA46623210425_072021-O.webp" 
+    },
+    { 
+        id:8,
+      name: "Impresora HP", 
+      precio: 18000, 
+      descripcion: "Laser Wifi Multifuncion", 
+      image:"https://http2.mlstatic.com/D_NQ_NP_833530-MLA40690953546_022020-O.webp" 
+    }, 
+    { 
+        id:9,
+      name: "Webcam Logitech", 
+      precio: 12000, 
+      descripcion: "Wifi 1080", 
+      image:"https://http2.mlstatic.com/D_NQ_NP_789405-MLA49253755126_032022-O.webp" 
+    },
+    { 
+        id:10,
+      name: "Memoria RAM Kingston 8GB", 
+      precio: 18000, 
+      descripcion: "DDR4", 
+      image:"https://http2.mlstatic.com/D_NQ_NP_935642-MLA49130955690_022022-O.webp" 
+    },
+    ]
+}
+
+  export default data;
+  
